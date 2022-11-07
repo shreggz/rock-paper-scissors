5bb6a59b8eef8cd342e2c1fb5e1d064ca622a1c2
Sridhar Bhagavathula <sridhar.bhagavathula@gmail.com>
2022-11-07 14:23:36 -0500

    Create function for player input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,22 @@
-const moveOptions = ["Rock", "Paper", "Scissors"];
+const moveOptions = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     const computerSelection = moveOptions[Math.floor(Math.random()*moveOptions.length)]
     return computerSelection;
 }
+
+function getPlayerChoice() {
+    let playerInput = prompt("Choose your weapon!");
+    while (playerInput == null) {
+        playerInput = prompt("No exit! Choose your weapon.")
+    }
+    let playerSelection = playerInput.toLowerCase();
+    console.log(playerSelection);
+}
+
+
+/* if (!moveOptions.includes(playerSelection)) {
+    prompt("Not a valid weapon. Choose rock, paper, or scissors.")
+} else {
+    return playerSelection;
+} */
